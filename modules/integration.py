65f619fcc2708b27597b50806c6a618adef1b4bf
javier-rozalen <82462569+javier-rozalen@users.noreply.github.com>
2022-05-06 15:31:44 +0200

    Add files via upload

---
+++ modules/integration.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May  3 10:07:40 2022
+
+@author: jozalen
+"""
+
+import torch
+
+def gl64(w_i,function):
+    """Integrates a function given via tensor by Gauss-Legendre"""
+    return torch.dot(function,w_i)
+
+def gl64_function(function,a,b,x_i,w_i):
+    """Returns the quadrature a function in an interval (a,b) given via tensor 
+    using the method of Gauss-Legendre with N=64"""
+    
+    # Parameters
+    a = torch.tensor(a)
+    b = torch.tensor(b)
+    k1 = (b-a)/2
+    k2 = (b+a)/2
+    gl64 = torch.tensor(0.)
+    c = 0
+
+    for i in range(64):
+        w_k = w_i[c]
+        x_k = k1*x_i[c]+k2
+        gl64 = gl64 + w_k*function(x_k.unsqueeze(0))
+        c += 1
+        
+    return gl64*k1
---
+++ modules/physical_constants.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May  3 10:05:32 2022
+
+@author: jozalen
+"""
+
+import math
+
+# Physical parameters
+mu = 938.9182 # MeV
+V_r = 1438.723 # MeV·fm
+V_a = 626.893 # MeV·fm
+hbar = 197.32968 # MeV·fm
+m_r = 3.11 # fm^-1
+m_a = 1.55 # fm^-1
+pi = math.pi
