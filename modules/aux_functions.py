f802281360dc799bcdcb4ec96ff52288861d384b
Javi Rozalén Sarmiento <82462569+javier-rozalen@users.noreply.github.com>
2024-07-30 13:22:23 +0200

    Add files via upload

--- modules/aux_functions.py
+++ modules/aux_functions.py
@@ -10,6 +10,7 @@ text.
 import re, os
 from math import log10, floor
 
+
 ############# FUNCTIONS #############
 def chunks(l, n):
     """
@@ -28,14 +29,15 @@ def chunks(l, n):
         Splitted list.
 
     """
-    n = max(1, n)   
-    splitted_list = [l[i:i+n] for i in range(0, len(l), n)]
+    n = max(1, n)
+    splitted_list = [l[i : i + n] for i in range(0, len(l), n)]
     return splitted_list
+
 
 def split(l, n):
     """
     Splits an input list l into n chunks.
-    
+
     Parameters
     ----------
     l : list
@@ -49,11 +51,12 @@ def split(l, n):
         Splitted list.
 
     """
-    n = min(n, len(l)) # don't create empty buckets
+    n = min(n, len(l))  # don't create empty buckets
     k, m = divmod(len(l), n)
-    splitted = (l[i*k+min(i, m):(i+1)*k+min(i+1, m)] for i in range(n))
+    splitted = (l[i * k + min(i, m) : (i + 1) * k + min(i + 1, m)] for i in range(n))
     splitted_list = list(splitted)
     return splitted_list
+
 
 def num_sort(test_string):
     """
@@ -70,9 +73,25 @@ def num_sort(test_string):
         Numerically-sorted list.
 
     """
-    sorted_list = list(map(int, re.findall(r'\d+', test_string)))[0]
-    
+    sorted_list = list(map(int, re.findall(r"\d+", test_string)))[0]
+
     return sorted_list
+
+
+def strtobool(val):
+    """Convert a string representation of truth to true (1) or false (0).
+    True values are 'y', 'yes', 't', 'true', 'on', and '1'; false values
+    are 'n', 'no', 'f', 'false', 'off', and '0'.  Raises ValueError if
+    'val' is anything else.
+    """
+    val = val.lower()
+    if val in ("y", "yes", "t", "true", "on", "1"):
+        return 1
+    elif val in ("n", "no", "f", "false", "off", "0"):
+        return 0
+    else:
+        raise ValueError("invalid truth value %r" % (val,))
+
 
 def get_keys_from_value(d, val):
     """
@@ -94,6 +113,7 @@ def get_keys_from_value(d, val):
     keys_list = [k for k, v in d.items() if v == val]
     return keys_list
 
+
 def show_layers(model):
     """
     Shows the layers of the input Neural Network model.
@@ -110,13 +130,13 @@ def show_layers(model):
     """
     print("\nLayers and parameters:\n")
     for name, param in model.named_parameters():
-        print(f"Layer: {name} | Size: {param.size()} | Values : " \
-              f"{param[:100]} \n")
-        
+        print(f"Layer: {name} | Size: {param.size()} | Values : " f"{param[:100]} \n")
+
+
 def dir_support(list_of_nested_dirs):
     """
-    Directories support: ensures that the (nested) directories given via the 
-    input list do exist, creating them if necessary. 
+    Directories support: ensures that the (nested) directories given via the
+    input list do exist, creating them if necessary.
 
     Parameters
     ----------
@@ -129,11 +149,12 @@ def dir_support(list_of_nested_dirs):
 
     """
     for i in range(len(list_of_nested_dirs)):
-        potential_dir = '/'.join(list_of_nested_dirs[:i+1]) 
+        potential_dir = "/".join(list_of_nested_dirs[: i + 1])
         if not os.path.exists(potential_dir):
             os.makedirs(potential_dir)
-            print(f'Creating directory {potential_dir}...')
-            
+            print(f"Creating directory {potential_dir}...")
+
+
 def round_to_1(x):
     """
     Rounds a number to the first decimal place. Useful for computing errors.
@@ -151,15 +172,27 @@ def round_to_1(x):
     """
     rounded_number = round(x, -int(floor(log10(abs(x)))))
     return rounded_number
-            
-def pretrain_loop(model, loss_fn, optimizer, train_data, q_2, arch, 
-                  integration, w_i, norm_s, norm_d, psi_ansatz_s, 
-                  psi_ansatz_d):
+
+
+def pretrain_loop(
+    model,
+    loss_fn,
+    optimizer,
+    train_data,
+    q_2,
+    arch,
+    integration,
+    w_i,
+    norm_s,
+    norm_d,
+    psi_ansatz_s,
+    psi_ansatz_d,
+):
     """
     Pretraining loop.
-    
+
     Parameters
-    ----------   
+    ----------
     model : object
         Neural Network (torch) instance.
     loss_fn : function
@@ -169,7 +202,7 @@ def pretrain_loop(model, loss_fn, optimizer, train_data, q_2, arch,
     train_data : tensor
         Training data.
     q_2 : tensor
-        Training data squared. We pass it as argument so as not to over-compute 
+        Training data squared. We pass it as argument so as not to over-compute
         it.
     arch : string
         Architecture among the 4 specified in the article.
@@ -182,10 +215,10 @@ def pretrain_loop(model, loss_fn, optimizer, train_data, q_2, arch,
     norm_d : tensor
         Norm of the ansatz S-state.
     psi_ansatz_s : tensor
-        Ansatz of the S-state. 
+        Ansatz of the S-state.
     psi_ansatz_d : tensor
         Ansatz of the D-state.
-        
+
     Returns
     -------
     psi_s_pred : tensor
@@ -199,27 +232,43 @@ def pretrain_loop(model, loss_fn, optimizer, train_data, q_2, arch,
 
     """
     optimizer.zero_grad()
-    (loss, psi_s_pred, psi_d_pred, 
-     k_s, k_d) = loss_fn(model=model, 
-                         arch=arch,
-                         train_data=train_data,
-                         norm_s = norm_s,
-                         norm_d = norm_d,
-                         q_2=q_2,
-                         integration=integration,
-                         w_i=w_i,
-                         psi_ansatz_s=psi_ansatz_s,
-                         psi_ansatz_d=psi_ansatz_d)
+    (loss, psi_s_pred, psi_d_pred, k_s, k_d) = loss_fn(
+        model=model,
+        arch=arch,
+        train_data=train_data,
+        norm_s=norm_s,
+        norm_d=norm_d,
+        q_2=q_2,
+        integration=integration,
+        w_i=w_i,
+        psi_ansatz_s=psi_ansatz_s,
+        psi_ansatz_d=psi_ansatz_d,
+    )
     loss.backward()
     optimizer.step()
-    
-    return psi_s_pred, psi_d_pred, k_s, k_d
 
-def train_loop(model, loss_fn, optimizer, train_data, q_2, arch, integration, 
-               w_i, q2_128, wi_128, V_ij, psi_exact_s, psi_exact_s_norm,
-               psi_exact_d, psi_exact_d_norm):
+    return psi_s_pred, psi_d_pred, k_s, k_d, loss
+
+
+def train_loop(
+    model,
+    loss_fn,
+    optimizer,
+    train_data,
+    q_2,
+    arch,
+    integration,
+    w_i,
+    q2_128,
+    wi_128,
+    V_ij,
+    psi_exact_s,
+    psi_exact_s_norm,
+    psi_exact_d,
+    psi_exact_d_norm,
+):
     """
-    Training loop. 
+    Training loop.
 
     Parameters
     ----------
@@ -232,7 +281,7 @@ def train_loop(model, loss_fn, optimizer, train_data, q_2, arch, integration,
     train_data : tensor
         Training data.
     q_2 : tensor
-        Training data squared. We pass it as argument so as not to over-compute 
+        Training data squared. We pass it as argument so as not to over-compute
         it.
     arch : string
         Architecture among the 4 specified in the article.
@@ -277,29 +326,40 @@ def train_loop(model, loss_fn, optimizer, train_data, q_2, arch, integration,
         Probability of the ANN D-state.
 
     """
-    
+
     optimizer.zero_grad()
-    (loss, ann_s, ann_d, norm2, 
-     K, U, E, ks, kd, pd) = loss_fn(model=model,
-                                    train_data=train_data, 
-                                    q_2=q_2, 
-                                    arch=arch, 
-                                    integration=integration,
-                                    w_i=w_i,
-                                    q2_128=q2_128,
-                                    wi_128=wi_128,
-                                    V_ij=V_ij,
-                                    psi_exact_s=psi_exact_s,
-                                    psi_exact_s_norm=psi_exact_s_norm,
-                                    psi_exact_d=psi_exact_d,
-                                    psi_exact_d_norm=psi_exact_d_norm)
+    (loss, ann_s, ann_d, norm2, K, U, E, ks, kd, pd) = loss_fn(
+        model=model,
+        train_data=train_data,
+        q_2=q_2,
+        arch=arch,
+        integration=integration,
+        w_i=w_i,
+        q2_128=q2_128,
+        wi_128=wi_128,
+        V_ij=V_ij,
+        psi_exact_s=psi_exact_s,
+        psi_exact_s_norm=psi_exact_s_norm,
+        psi_exact_d=psi_exact_d,
+        psi_exact_d_norm=psi_exact_d_norm,
+    )
     loss.backward()
     optimizer.step()
-    
+
     return loss, ann_s, ann_d, norm2, K, U, ks, kd, pd
 
-def f(E_accum, ks_accum, kd_accum, pd_accum, print_stats_for_every_model,
-      mean_E, mean_ks, mean_kd, mean_PD):  
+
+def f(
+    E_accum,
+    ks_accum,
+    kd_accum,
+    pd_accum,
+    print_stats_for_every_model,
+    mean_E,
+    mean_ks,
+    mean_kd,
+    mean_PD,
+):
     """
     Computes top and bottom errors of input values, and prints this information
     to console.
@@ -344,57 +404,88 @@ def f(E_accum, ks_accum, kd_accum, pd_accum, print_stats_for_every_model,
     mean_pd_bot : float
         Mean of the D-state probability lower bounds.
 
-    """     
-    
+    """
+
     mean_E_top = max(E_accum)
     mean_E_bot = min(E_accum)
-    
+
     mean_ks_top = max(ks_accum)
     mean_ks_bot = min(ks_accum)
-    
+
     mean_kd_top = max(kd_accum)
     mean_kd_bot = min(kd_accum)
-    
+
     mean_pd_top = max(pd_accum)
     mean_pd_bot = min(pd_accum)
-               
+
     if print_stats_for_every_model == True:
-        E_plus = round_to_1(abs(abs(mean_E_top)-abs(mean_E)))
-        E_plus_error = mean_E+round_to_1(abs(abs(mean_E_top)-abs(mean_E)))
-        E_minus = round_to_1(abs(abs(mean_E_bot)-abs(mean_E)))
-        E_minus_error = mean_E-round_to_1(abs(abs(mean_E_bot)-abs(mean_E)))
-        Ks_plus = round_to_1(abs(abs(mean_ks_top)-abs(mean_ks)))
-        Ks_minus = round_to_1(abs(abs(mean_ks_bot)-abs(mean_ks)))
-        Kd_plus = round_to_1(abs(abs(mean_kd_top)-abs(mean_kd)))
-        Kd_minus = round_to_1(abs(abs(mean_kd_bot)-abs(mean_kd)))
-        Pd_plus = round_to_1(abs(abs(mean_pd_top)-abs(mean_PD)))
-        Pd_minus = round_to_1(abs(abs(mean_pd_bot)-abs(mean_PD)))
+        E_plus = round_to_1(abs(abs(mean_E_top) - abs(mean_E)))
+        E_plus_error = mean_E + round_to_1(abs(abs(mean_E_top) - abs(mean_E)))
+        E_minus = round_to_1(abs(abs(mean_E_bot) - abs(mean_E)))
+        E_minus_error = mean_E - round_to_1(abs(abs(mean_E_bot) - abs(mean_E)))
+        Ks_plus = round_to_1(abs(abs(mean_ks_top) - abs(mean_ks)))
+        Ks_minus = round_to_1(abs(abs(mean_ks_bot) - abs(mean_ks)))
+        Kd_plus = round_to_1(abs(abs(mean_kd_top) - abs(mean_kd)))
+        Kd_minus = round_to_1(abs(abs(mean_kd_bot) - abs(mean_kd)))
+        Pd_plus = round_to_1(abs(abs(mean_pd_top) - abs(mean_PD)))
+        Pd_minus = round_to_1(abs(abs(mean_pd_bot) - abs(mean_PD)))
         print("\nE = {:16.6f}".format(mean_E))
         print("E+ = {}, {}".format(E_plus, E_plus_error))
         print("E- = {}, {}".format(E_minus, E_minus_error))
-        
+
         print("\nKs = {:16.6f}".format(mean_ks))
         print("Ks+ = {}".format(Ks_plus))
         print("Ks- = {}".format(Ks_minus))
-        
+
         print("\nKd = {:16.6f}".format(mean_kd))
         print("Kd+ = {}".format(Kd_plus))
         print("Kd- = {}".format(Kd_minus))
-        
+
         print("\nPd = {:16.6f}".format(mean_PD))
         print("Pd+ = {}".format(Pd_plus))
         print("Pd- = {}".format(Pd_minus))
-    
-    return (mean_E_top, mean_E_bot, mean_ks_top, mean_ks_bot, mean_kd_top,
-            mean_kd_bot, mean_pd_top, mean_pd_bot)
 
-def error_update(E_accum, mean_E_accum, E_top_accum, E_bot_accum, 
-                 mean_E_top, mean_E_bot, mean_ks_top, mean_ks_bot, 
-                 mean_kd_top, mean_kd_bot, mean_pd_top, mean_pd_bot, 
-                 mean_ks, ks_accum, 
-                 mean_ks_accum, ks_top_accum, ks_bot_accum, kd_accum,
-                 mean_kd, mean_kd_accum, kd_top_accum, kd_bot_accum, pd_accum,
-                 mean_PD, mean_PD_accum, pd_top_accum, pd_bot_accum):
+    return (
+        mean_E_top,
+        mean_E_bot,
+        mean_ks_top,
+        mean_ks_bot,
+        mean_kd_top,
+        mean_kd_bot,
+        mean_pd_top,
+        mean_pd_bot,
+    )
+
+
+def error_update(
+    E_accum,
+    mean_E_accum,
+    E_top_accum,
+    E_bot_accum,
+    mean_E_top,
+    mean_E_bot,
+    mean_ks_top,
+    mean_ks_bot,
+    mean_kd_top,
+    mean_kd_bot,
+    mean_pd_top,
+    mean_pd_bot,
+    mean_ks,
+    ks_accum,
+    mean_ks_accum,
+    ks_top_accum,
+    ks_bot_accum,
+    kd_accum,
+    mean_kd,
+    mean_kd_accum,
+    kd_top_accum,
+    kd_bot_accum,
+    pd_accum,
+    mean_PD,
+    mean_PD_accum,
+    pd_top_accum,
+    pd_bot_accum,
+):
     """
     Updates the different error quantities with information from the current
     iteration.
@@ -507,70 +598,92 @@ def error_update(E_accum, mean_E_accum, E_top_accum, E_bot_accum,
     mean_pd_bot : float
         Mean of the D-state probability lower bounds.
     break_ : Boolean
-        Whether to break out of the loop in the main program or not. The 
+        Whether to break out of the loop in the main program or not. The
         condition for breaking is having 'mean_E' <= 0.
 
     """
 
     break_ = False
     # Energy
-    mean_E = sum(E_accum)/len(E_accum)
+    mean_E = sum(E_accum) / len(E_accum)
     mean_E_accum.append(mean_E)
     candidate_to_extreme = E_accum[-4]
-    if E_accum[-2]>E_accum[-1] and E_accum[-2]>E_accum[-3]:
+    if E_accum[-2] > E_accum[-1] and E_accum[-2] > E_accum[-3]:
         E_top_accum.append(candidate_to_extreme)
-    elif E_accum[-2]<E_accum[-1] and E_accum[-2]<E_accum[-3]:
+    elif E_accum[-2] < E_accum[-1] and E_accum[-2] < E_accum[-3]:
         E_bot_accum.append(candidate_to_extreme)
     if len(E_top_accum) > 0:
-        mean_E_top = sum(E_top_accum)/len(E_top_accum)
+        mean_E_top = sum(E_top_accum) / len(E_top_accum)
     if len(E_bot_accum) > 0:
-        mean_E_bot = sum(E_bot_accum)/len(E_bot_accum)
-        
-    if mean_E <= 0.:
+        mean_E_bot = sum(E_bot_accum) / len(E_bot_accum)
+
+    if mean_E <= 0.0:
         # Overlap S
-        mean_ks = sum(ks_accum)/len(ks_accum)
+        mean_ks = sum(ks_accum) / len(ks_accum)
         mean_ks_accum.append(mean_ks)
         candidate_to_extreme = ks_accum[-2]
-        if ks_accum[-2]>ks_accum[-1] and ks_accum[-2]>ks_accum[-3]:
+        if ks_accum[-2] > ks_accum[-1] and ks_accum[-2] > ks_accum[-3]:
             ks_top_accum.append(candidate_to_extreme)
-        elif ks_accum[-2]<ks_accum[-1] and ks_accum[-2]<ks_accum[-3]:
+        elif ks_accum[-2] < ks_accum[-1] and ks_accum[-2] < ks_accum[-3]:
             ks_bot_accum.append(candidate_to_extreme)
         if len(ks_top_accum) > 0:
-            mean_ks_top = sum(ks_top_accum)/len(ks_top_accum)
+            mean_ks_top = sum(ks_top_accum) / len(ks_top_accum)
         if len(ks_bot_accum) > 0:
-            mean_ks_bot = sum(ks_bot_accum)/len(ks_bot_accum)
-            
+            mean_ks_bot = sum(ks_bot_accum) / len(ks_bot_accum)
+
         # Overlap D
-        mean_kd = sum(kd_accum)/len(kd_accum)
+        mean_kd = sum(kd_accum) / len(kd_accum)
         mean_kd_accum.append(mean_kd)
         candidate_to_extreme = kd_accum[-2]
-        if kd_accum[-2]>kd_accum[-1] and kd_accum[-2]>kd_accum[-3]:
+        if kd_accum[-2] > kd_accum[-1] and kd_accum[-2] > kd_accum[-3]:
             kd_top_accum.append(candidate_to_extreme)
-        elif kd_accum[-2]<kd_accum[-1] and kd_accum[-2]<kd_accum[-3]:
+        elif kd_accum[-2] < kd_accum[-1] and kd_accum[-2] < kd_accum[-3]:
             kd_bot_accum.append(candidate_to_extreme)
         if len(kd_top_accum) > 0:
-            mean_kd_top = sum(kd_top_accum)/len(kd_top_accum)
+            mean_kd_top = sum(kd_top_accum) / len(kd_top_accum)
         if len(kd_bot_accum) > 0:
-            mean_kd_bot = sum(kd_bot_accum)/len(kd_bot_accum)        
+            mean_kd_bot = sum(kd_bot_accum) / len(kd_bot_accum)
 
         # Prob. of D_state
-        mean_PD = sum(pd_accum)/len(pd_accum)
+        mean_PD = sum(pd_accum) / len(pd_accum)
         mean_PD_accum.append(mean_PD)
         candidate_to_extreme = pd_accum[-2]
-        if pd_accum[-2]>pd_accum[-1] and pd_accum[-2]>pd_accum[-3]:
+        if pd_accum[-2] > pd_accum[-1] and pd_accum[-2] > pd_accum[-3]:
             pd_top_accum.append(candidate_to_extreme)
-        elif pd_accum[-2]<pd_accum[-1] and pd_accum[-2]<pd_accum[-3]:
+        elif pd_accum[-2] < pd_accum[-1] and pd_accum[-2] < pd_accum[-3]:
             pd_bot_accum.append(candidate_to_extreme)
         if len(pd_top_accum) > 0:
-            mean_pd_top = sum(pd_top_accum)/len(pd_top_accum)
+            mean_pd_top = sum(pd_top_accum) / len(pd_top_accum)
         if len(pd_bot_accum) > 0:
-            mean_pd_bot = sum(pd_bot_accum)/len(pd_bot_accum)
-    else: 
+            mean_pd_bot = sum(pd_bot_accum) / len(pd_bot_accum)
+    else:
         # print('Skipping non-convergent model...')
         break_ = True
-        
-    return (mean_E, mean_E_accum, E_top_accum, E_bot_accum, mean_E_top, 
-            mean_E_bot, mean_ks, mean_ks_accum, ks_top_accum, ks_bot_accum, 
-            mean_ks_top, mean_ks_bot, mean_kd, mean_kd_accum, kd_top_accum, 
-            kd_bot_accum, mean_kd_top, mean_kd_bot, mean_PD, mean_PD_accum, 
-            pd_top_accum, pd_bot_accum, mean_pd_top, mean_pd_bot, break_)
+
+    return (
+        mean_E,
+        mean_E_accum,
+        E_top_accum,
+        E_bot_accum,
+        mean_E_top,
+        mean_E_bot,
+        mean_ks,
+        mean_ks_accum,
+        ks_top_accum,
+        ks_bot_accum,
+        mean_ks_top,
+        mean_ks_bot,
+        mean_kd,
+        mean_kd_accum,
+        kd_top_accum,
+        kd_bot_accum,
+        mean_kd_top,
+        mean_kd_bot,
+        mean_PD,
+        mean_PD_accum,
+        pd_top_accum,
+        pd_bot_accum,
+        mean_pd_top,
+        mean_pd_bot,
+        break_,
+    )
